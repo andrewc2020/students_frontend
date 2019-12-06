8884ac4193b45d7d137de997dab8ee3cbf0dcbce
andrewc2020 <andrewc2020@gmail.com>
2019-12-06 09:45:20 +0000

    removed userpage

--- src/App.test.js
+++ src/App.test.js
@@ -8,7 +8,7 @@ import {userService} from './_services/user-service';
 import {ConsoleLog} from './_helpers/logger';
 
 
-import UserPage from './userPage/UserPage';
+
 import { authenticationService } from './_services/';
 
 
@@ -18,12 +18,7 @@ it('renders without crashing', () => {
   ReactDOM.render(<App />, div);
   ReactDOM.unmountComponentAtNode(div);
 });
-it('adds a dynamic class',()=>{
-  const div = document.createElement('div');
-  ReactDOM.render(<UserPage />, div);
-  ReactDOM.unmountComponentAtNode(div);
 
-})
 it('can import authenticationService',()=>{
   authenticationService.login('Saladin Sebag', 'salam').then((value)=>{
     console.log(value.user);
